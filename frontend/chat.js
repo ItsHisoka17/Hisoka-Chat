e474812f54d33134a61888cee8dbdd899bfe51da
Hisoka Morrow <61894183+ItsHisoka17@users.noreply.github.com>
2021-08-20 21:14:05 +0000

    DataHandling - Users (upcoming: Message), support for images, style improvements

--- frontend/chat.js
+++ frontend/chat.js
@@ -1,5 +1,5 @@
 let socket = io();
-
+let users = [];
 
 function handleUserName(){
 let name;
@@ -17,6 +17,12 @@ function onMessage(){
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     let { value } = document.getElementById('message');
+    let regex = /^.*(n(\s{1,2}|.{1,2})?(\s{1,2}|.{1,2})?i(\s{1,2}|.{1,2})?g(\s{1,2}|.{1,2})?g(\s{1,2}|.{1,2})?e(\s{1,2}|.{1,2})?r(\s{1,2}|.{1,2})?).*$/ig;
+    if (regex.exec(value)){
+      alert('Slurs are banned in this chat room');
+      document.getElementById('message').value = '';
+      return;
+    };
     socket.emit('message', {user: socket.username, message: value});
     document.getElementById('message').value = '';
   })
@@ -25,6 +31,16 @@ function onMessage(){
     socket.on('message', data => {
       let { user, message } = data;
       let div = document.createElement('div');
+      let image_regex = /^.*(https?:\/\/.*\.(png|webp|gif|jpg|jpeg)(\?.+)?)$/gm;
+      let test = image_regex.exec(message);
+      if (test){
+        div.innerHTML = `<br></br><p style="size: 9px;color: grey;">${user} - ${new Date().toTimeString().split(' ')[0].replace(/00/, '12').slice(0, 5)}</p><img src="${message}" style="width: 25%; height: 20%; border-radius: 5%;"></img>`;
+        let li = document.createElement('li');
+        li.appendChild(div);
+        let chat = document.getElementById('chat');
+        chat.appendChild(li);
+        return false;
+      }
       div.innerHTML = `<br></br><p style="size: 9px;color: grey;">${user} - ${new Date().toTimeString().split(' ')[0].replace(/00/, '12').slice(0, 5)}</p>\n<p>${message}</p>`;
       let li = document.createElement('li');
       li.appendChild(div);
@@ -37,8 +53,11 @@ function onMessage(){
 }
 
 function init(){
+  setTimeout(() => console.log(users), 10000)
   handleUserName();
   onMessage();
 };
 
 init();
+
+//Created by ItsHisoka17
--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const { User } = require('./backend/DataHandler');
+const userHandler = new User();
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/frontend/main.html'));
 
 app.use('/static', express.static(require('path').join(__dirname, 'frontend')))
-
 io.on('connection', (socket) => {
   socket.on('join', (data) => {
   io.sockets.emit('message', {user: 'System', message: `${data.name} Has Joined`});
   socket.username = data.name;
-  })  
+  userHandler.addUser({username: data.name, socket});
+  })
   socket.on('message', (data) => {
     io.sockets.emit('message', {user: data.user, message: data.message});
   })
   socket.on('disconnect', () => {
     io.sockets.emit('message', {user: 'System', message: `${socket.username} Has Disconnected`})
+    userHandler.removeUser({username: socket.username, socket});
   })
 });
 
 server.listen(80);
+
+//Created By ItsHisoka17
